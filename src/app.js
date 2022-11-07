f1fb9e00934ff1ce908294560fb0f8c08db7a0fb
dccuong <cuongksv10@gmail.com>
2022-11-07 09:58:28 +0700

    connect cloud.mongodb

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
 import { readdirSync } from "fs";
-// require("dotenv").config();
+require("dotenv").config();
 
 const app = express();
 
@@ -18,25 +18,25 @@ readdirSync("./src/routes").forEach((route) => {
 });
 
 // // connect db
-// mongoose
-//   .connect(process.env.DB_URL)
-//   .then(() => console.log("Connect successfully"))
-//   .catch((error) => console.log(error));
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Connect successfully"))
+  .catch((error) => console.log(error));
 
-// const PORT = process.env.PORT || 8080;
-// app.listen(PORT, () => console.log("Server is running on port", PORT));
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log("Server is running on port", PORT));
 
 // app.use("/api", productRouter)
 // app.use("/api", authRouter)
 // app.use("/api", cateRouter)
 // //connection db
-mongoose
-  .connect("mongodb://127.0.0.1:27017/we16304")
-  .then(() => console.log("kets noi thanh cong"))
-  .catch((error) => console.log(error));
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/we16304")
+//   .then(() => console.log("kets noi thanh cong"))
+//   .catch((error) => console.log(error));
 
-// connect
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log("Server của bạn đang chạy cổng ", PORT);
-});
+// // connect
+// const PORT = process.env.PORT || 8000;
+// app.listen(PORT, () => {
+//   console.log("Server của bạn đang chạy cổng ", PORT);
+// });
