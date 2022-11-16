935fb4450cf137d760ebf1ae7361b1c0fa73890c
lamltph17490 <106149403+lamltph17490@users.noreply.github.com>
2022-11-16 14:13:56 +0700

    first commit

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
 import { readdirSync } from "fs";
-import authRoute from "./routes/authRoutes";
 require("dotenv").config();
 
 const app = express();
 
+// middleware
 var bodyParser = require("body-parser");
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
@@ -20,25 +20,11 @@ readdirSync("./src/routes").forEach((route) => {
   app.use("/api", require(`./routes/${route}`));
 });
 
-app.use("/api", authRoute);
-
-// //connection db
-// // connect db
+// connect db
 mongoose
   .connect(process.env.DB_URL)
   .then(() => console.log("Connect successfully"))
   .catch((error) => console.log(error));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("Server is running on port", PORT));
-
-// mongoose
-//   .connect("mongodb://127.0.0.1:27017/we16304")
-//   .then(() => console.log("kets noi thanh cong"))
-//   .catch((error) => console.log(error));
-
-// // connect
-// const PORT = process.env.PORT || 8000;
-// app.listen(PORT, () => {
-//   console.log("Server của bạn đang chạy cổng ", PORT);
-// });
